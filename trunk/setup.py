3cad0e85ecd7c08ab7c126c4ec59e110514ce609
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2001-05-07 04:17:20 +0000

    Timothy Docker supplied distutils files.

---
+++ trunk/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# To use:
+#       python setup.py install
+#
+
+import distutils, os
+from distutils.core import setup, Extension
+
+sybase = '/opt/sybase'
+syb_incdir = os.path.join( sybase, 'include' )
+syb_libdir = '/opt/sybase/lib'
+syb_libs = [ 'blk', 'ct', 'cs', 'sybtcl', 'comn', 'intl' ]
+
+# The version is set in Lib/numeric_version.py
+
+setup (name = "Sybase",
+       version = "0.24",
+       maintainer = "Dave Cole",
+       maintainer_email = " djc@object-craft.com.au",
+       description = "Sybase Extension to Python",
+       url = "http://www.object-craft.com.au/projects/sybase/",
+       py_modules = ['Sybase'],
+       include_dirs = [syb_incdir],
+       ext_modules = [
+           Extension('sybasect',
+                     ['blk.c', 'buffer.c', 'cmd.c', 'conn.c', 'ctx.c',
+                      'datafmt.c', 'msgs.c', 'numeric.c', 'sybasect.c'],
+                     libraries = syb_libs,
+                     library_dirs = [syb_libdir]
+                     )
+           ],
+       )
+
