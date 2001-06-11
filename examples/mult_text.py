3c6195514255996442cabc5de488976fd12b2748
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2001-06-11 11:54:12 +0000

    More example work - need to set up pubs2 database to test cursor_sel.py

--- examples/mult_text.py
+++ examples/mult_text.py
@@ -24,30 +24,30 @@ def connect_db(ctx, user_name, password):
         raise Error("ct_con_alloc failed")
     status = conn.ct_con_props(CS_SET, CS_USERNAME, user_name)
     if status != CS_SUCCEED:
-        raise SybaseError(conn, "ct_con_props failed")
+        raise CTError(conn, "ct_con_props failed")
     status = conn.ct_con_props(CS_SET, CS_PASSWORD, password)
     if status != CS_SUCCEED:
-        raise SybaseError(conn, "ct_con_props failed")
+        raise CTError(conn, "ct_con_props failed")
     status = conn.ct_connect()
     if status != CS_SUCCEED:
-        raise SybaseError(conn, "ct_connect failed")
+        raise CTError(conn, "ct_connect failed")
     return conn
 
 def send_sql(cmd, sql):
     print "Command = %s" % sql
     status = cmd.ct_command(CS_LANG_CMD, sql)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_command failed")
+        raise CTError(cmd.conn, "ct_command failed")
     status = cmd.ct_send()
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_send failed")
+        raise CTError(cmd.conn, "ct_send failed")
 
 MAX_COLSIZE = 255
 
 def ProcessTimestamp(cmd, textdata):
     status, fmt = cmd.ct_describe(1)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_describe failed")
+        raise CTError(cmd.conn, "ct_describe failed")
     # Check if the data is a timestamp. If so, save it to the
     # CS_IODESC structure for future text updates.
     if not (fmt.status & CS_TIMESTAMP):
@@ -58,33 +58,33 @@ def ProcessTimestamp(cmd, textdata):
     fmt.format = CS_FMT_UNUSED;
     status, buf = cmd.ct_bind(1, fmt)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_bind failed")
+        raise CTError(cmd.conn, "ct_bind failed")
     # Retrieve the parameter result containing the timestamp.
     status, rows_read = cmd.ct_fetch()
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_fetch failed")
+        raise CTError(cmd.conn, "ct_fetch failed")
     # The timestamp was retrieved, so cancel the rest of the result set.
     status = cmd.ct_cancel(CS_CANCEL_CURRENT)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_cancel failed")
+        raise CTError(cmd.conn, "ct_cancel failed")
 
 def updatetextdata(textdata, iodesc, newdata):
     # Allocate a command handle to send the text with
     status, cmd = conn2.ct_cmd_alloc()
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_cmd_alloc failed")
+        raise CTError(cmd.conn, "ct_cmd_alloc failed")
     # Inform Client-Library the next data sent will be used for a text
     # or image update.
     status = cmd.ct_command(CS_SEND_DATA_CMD)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_command failed")
+        raise CTError(cmd.conn, "ct_command failed")
     # Fill in the description information for the update and send it
     # to Client-Library.
     iodesc.total_txtlen = len(newdata)
     iodesc.log_on_update = CS_TRUE
     status = cmd.ct_data_info(CS_SET, CS_UNUSED, iodesc)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_data_info failed")
+        raise CTError(cmd.conn, "ct_data_info failed")
     # Send the text one byte at a time. This is not the best thing to
     # do for performance reasons, but does demonstrate the
     # ct_send_data() can handle arbitrary amounts of data.
@@ -95,13 +95,13 @@ def updatetextdata(textdata, iodesc, newdata):
         buf[0] = c
         status = cmd.ct_send_data(buf)
         if status != CS_SUCCEED:
-            raise SybaseError(cmd.conn, "ct_send_data failed")
+            raise CTError(cmd.conn, "ct_send_data failed")
     # ct_send_data() does writes to internal network buffers. To
     # insure that all the data is flushed to the server, a ct_send()
     # is done.
     status = cmd.ct_send()
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_send failed")
+        raise CTError(cmd.conn, "ct_send failed")
     # Process the results of the command
     while 1:
         status, result = cmd.ct_results()
@@ -117,7 +117,7 @@ def updatetextdata(textdata, iodesc, newdata):
             # cancel the current result set.
             status = cmd.ct_cancel(CS_CANCEL_CURRENT)
             if status != CS_SUCCEED:
-                raise SybaseError(cmd.conn, "ct_cancel failed")
+                raise CTError(cmd.conn, "ct_cancel failed")
         elif result in (CS_CMD_SUCCEED, CS_CMD_DONE):
             # This means that the command succeeded or is finished.
             pass
@@ -146,7 +146,7 @@ def updatetextdata(textdata, iodesc, newdata):
 def fetch_n_print(cmd):
     status, num_cols = cmd.ct_res_info(CS_NUMDATA)
     if status != CS_SUCCEED:
-        raise SybaseError(cmd.conn, "ct_res_info failed")
+        raise CTError(cmd.conn, "ct_res_info failed")
     fmt = CS_DATAFMT()
     fmt.datatype = CS_CHAR_TYPE
     fmt.maxlength = MAX_COLSIZE
@@ -173,10 +173,10 @@ def fetch_n_print(cmd):
         text = string.join(parts, '')
         print "Length of first text column = %d" % len(text)
         if status != CS_END_ITEM:
-            raise SybaseError(cmd.conn, "ct_get_data failed")
+            raise CTError(cmd.conn, "ct_get_data failed")
         status, iodesc = cmd.ct_data_info(CS_GET, 2)
         if status != CS_SUCCEED:
-            raise SybaseError(cmd.conn, "ct_data_info failed")
+            raise CTError(cmd.conn, "ct_data_info failed")
         print "\n\n The text data retrieved is: %s" % text
 
         print "Updating first text column .."
@@ -193,10 +193,10 @@ def fetch_n_print(cmd):
         text2 = string.join(parts, '')
         print "Length of 2nd text column = %d" % len(text2)
         if status != CS_END_DATA:
-            raise SybaseError(cmd.conn, "ct_get_data failed")
+            raise CTError(cmd.conn, "ct_get_data failed")
         status, iodesc = cmd.ct_data_info(CS_GET, 3)
         if status != CS_SUCCEED:
-            raise SybaseError(cmd.conn, "ct_data_info failed")
+            raise CTError(cmd.conn, "ct_data_info failed")
         print "\n\n The text data retrieved is: %s" % text2
 
         print "Updating second text column .."
@@ -223,14 +223,14 @@ def handle_returns(cmd):
             return
     if status == CS_END_RESULTS:
         return CS_SUCCEED
-    raise SybaseError(cmd.conn, "ct_results failed")
+    raise CTError(cmd.conn, "ct_results failed")
 
 ctx = init_db()
 conn = connect_db(ctx, EX_USERNAME, EX_PASSWORD)
 conn2 = connect_db(ctx, EX_USERNAME, EX_PASSWORD)
 status, cmd = conn.ct_cmd_alloc()
 if status != CS_SUCCEED:
-    raise SybaseError(conn, "ct_cmd_alloc failed")
+    raise CTError(conn, "ct_cmd_alloc failed")
 send_sql(cmd, "insert tempdb..test values(1,'hello world','hello worl')")
 handle_returns(cmd)
 send_sql(cmd, "select * from tempdb..test")
