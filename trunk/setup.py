273b01cd3a8ff2d0f31cba4baafe9aae557ca42f
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2001-06-19 02:42:23 +0000

    Use same setup file for all platforms.

--- trunk/setup.py
+++ trunk/setup.py
@@ -6,17 +6,31 @@
 import distutils, os
 from distutils.core import setup, Extension
 
-sybase = '/opt/sybase'
+if os.name == 'posix':
+    sybase = '/opt/sybase'
+    syb_libs = [ 'blk', 'ct', 'cs', 'sybtcl', 'comn', 'intl' ]
+    mxdatetime_incdir = os.path.join(os.environ['HOME'],
+                                     'download/DateTime/mxDateTime')
+elif os.name == 'nt':
+    sybase = 'i:\\sybase\\sql11.5'
+    syb_libs = [ 'libblk', 'libct', 'libcs' ]
+    mxdatetime_incdir = 'i:\\download\\DateTime\\mxDateTime'
+else:
+    import sys
+    sys.stderr.write(
+        'Sorry, I do not know how to build on this platform.\n'
+        'Please edit setup.py and add platform specific settings.  If you'
+        'figure out how to get it working for your platform, please send'
+        'mail to djc@object-craft.com.au so you can help other people.\n')
+    sys.exit(1)
+
 syb_incdir = os.path.join( sybase, 'include' )
 syb_libdir = os.path.join( sybase, 'lib' )
-syb_libs = [ 'blk', 'ct', 'cs', 'sybtcl', 'comn', 'intl' ]
-
-mxdatetime_incdir = '/home/djc/download/DateTime/mxDateTime'
 
 # The version is set in Lib/numeric_version.py
 
 setup (name = "Sybase",
-       version = "0.24",
+       version = "0.26",
        maintainer = "Dave Cole",
        maintainer_email = " djc@object-craft.com.au",
        description = "Sybase Extension to Python",
