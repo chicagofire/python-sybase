e12798634b7c1040ddd9ebc1101e946f623862e5
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2001-09-15 03:00:10 +0000

    Ensure module compiles with older versions.

--- trunk/conn.c
+++ trunk/conn.c
@@ -371,72 +371,176 @@ static PyObject *CS_CONNECTION_ct_con_drop(CS_CONNECTIONObj *self, PyObject *arg
 static int property_type(int property)
 {
     switch (property) {
+#ifdef CS_ANSI_BINDS
     case CS_ANSI_BINDS:
+#endif
+#ifdef CS_ASYNC_NOTIFS
     case CS_ASYNC_NOTIFS:
+#endif
+#ifdef CS_BULK_LOGIN
     case CS_BULK_LOGIN:
+#endif
+#ifdef CS_CHARSETCNV
     case CS_CHARSETCNV:
+#endif
+#ifdef CS_CONFIG_BY_SERVERNAME
     case CS_CONFIG_BY_SERVERNAME:
+#endif
+#ifdef CS_DIAG_TIMEOUT
     case CS_DIAG_TIMEOUT:
+#endif
+#ifdef CS_DISABLE_POLL
     case CS_DISABLE_POLL:
+#endif
+#ifdef CS_DS_COPY
     case CS_DS_COPY:
+#endif
+#ifdef CS_DS_EXPANDALIAS
     case CS_DS_EXPANDALIAS:
+#endif
+#ifdef CS_DS_FAILOVER
     case CS_DS_FAILOVER:
+#endif
     case CS_EXPOSE_FMTS:
+#ifdef CS_EXTERNAL_CONFIG
     case CS_EXTERNAL_CONFIG:
+#endif
+#ifdef CS_EXTRA_INF
     case CS_EXTRA_INF:
+#endif
+#ifdef CS_HIDDEN_KEYS
     case CS_HIDDEN_KEYS:
+#endif
+#ifdef CS_LOGIN_STATUS
     case CS_LOGIN_STATUS:
+#endif
+#ifdef CS_NOCHARSETCNV_REQD
     case CS_NOCHARSETCNV_REQD:
+#endif
+#ifdef CS_SEC_APPDEFINED
     case CS_SEC_APPDEFINED:
+#endif
+#ifdef CS_SEC_CHALLENGE
     case CS_SEC_CHALLENGE:
+#endif
+#ifdef CS_SEC_CHANBIND
     case CS_SEC_CHANBIND:
+#endif
+#ifdef CS_SEC_CONFIDENTIALITY
     case CS_SEC_CONFIDENTIALITY:
+#endif
+#ifdef CS_SEC_DATAORIGIN
     case CS_SEC_DATAORIGIN:
+#endif
+#ifdef CS_SEC_DELEGATION
     case CS_SEC_DELEGATION:
+#endif
+#ifdef CS_SEC_DETECTREPLAY
     case CS_SEC_DETECTREPLAY:
+#endif
+#ifdef CS_SEC_DETECTSEQ
     case CS_SEC_DETECTSEQ:
+#endif
+#ifdef CS_SEC_ENCRYPTION
     case CS_SEC_ENCRYPTION:
+#endif
+#ifdef CS_SEC_INTEGRITY
     case CS_SEC_INTEGRITY:
+#endif
+#ifdef CS_SEC_MUTUALAUTH
     case CS_SEC_MUTUALAUTH:
+#endif
+#ifdef CS_SEC_NEGOTIATE
     case CS_SEC_NEGOTIATE:
+#endif
+#ifdef CS_SEC_NETWORKAUTH
     case CS_SEC_NETWORKAUTH:
+#endif
 	return OPTION_BOOL;
+#ifdef CS_CON_STATUS
     case CS_CON_STATUS:
+#endif
 #ifdef CS_LOOP_DELAY
     case CS_LOOP_DELAY:
 #endif
 #ifdef CS_RETRY_COUNT
     case CS_RETRY_COUNT:
 #endif
+#ifdef CS_NETIO
     case CS_NETIO:
+#endif
+#ifdef CS_TEXTLIMIT
     case CS_TEXTLIMIT:
+#endif
+#ifdef CS_DS_SEARCH
     case CS_DS_SEARCH:
+#endif
+#ifdef CS_DS_SIZELIMIT
     case CS_DS_SIZELIMIT:
+#endif
+#ifdef CS_DS_TIMELIMIT
     case CS_DS_TIMELIMIT:
+#endif
+#ifdef CS_ENDPOINT
     case CS_ENDPOINT:
+#endif
+#ifdef CS_PACKETSIZE
     case CS_PACKETSIZE:
+#endif
+#ifdef CS_SEC_CREDTIMEOUT
     case CS_SEC_CREDTIMEOUT:
+#endif
+#ifdef CS_SEC_SESSTIMEOUT
     case CS_SEC_SESSTIMEOUT:
+#endif
+#ifdef CS_TDS_VERSION
     case CS_TDS_VERSION:
+#endif
 	return OPTION_INT;
+#ifdef CS_APPNAME
     case CS_APPNAME:
+#endif
+#ifdef CS_HOSTNAME
     case CS_HOSTNAME:
+#endif
+#ifdef CS_PASSWORD
     case CS_PASSWORD:
+#endif
+#ifdef CS_SERVERNAME
     case CS_SERVERNAME:
+#endif
+#ifdef CS_USERNAME
     case CS_USERNAME:
+#endif
+#ifdef CS_DS_DITBASE
     case CS_DS_DITBASE:
+#endif
 #ifdef CS_DS_PASSWORD
     case CS_DS_PASSWORD:
 #endif
+#ifdef CS_DS_PRINCIPAL
     case CS_DS_PRINCIPAL:
+#endif
+#ifdef CS_DS_PROVIDER
     case CS_DS_PROVIDER:
+#endif
+#ifdef CS_SEC_KEYTAB
     case CS_SEC_KEYTAB:
+#endif
+#ifdef CS_SEC_MECHANISM
     case CS_SEC_MECHANISM:
+#endif
+#ifdef CS_SEC_SERVERPRINCIPAL
     case CS_SEC_SERVERPRINCIPAL:
+#endif
+#ifdef CS_TRANSACTION_NAME
     case CS_TRANSACTION_NAME:
+#endif
 	return OPTION_STRING;
+#ifdef CS_EED_CMD
     case CS_EED_CMD:
 	return OPTION_CMD;
+#endif
     default:
 	return OPTION_UNKNOWN;
     }
@@ -637,39 +741,101 @@ static PyObject *CS_CONNECTION_ct_con_props(CS_CONNECTIONObj *self, PyObject *ar
 static int option_type(int option)
 {
     switch (option) {
+#ifdef CS_OPT_ANSINULL
     case CS_OPT_ANSINULL:
+#endif
+#ifdef CS_OPT_ANSIPERM
     case CS_OPT_ANSIPERM:
+#endif
+#ifdef CS_OPT_ARITHABORT
     case CS_OPT_ARITHABORT:
+#endif
+#ifdef CS_OPT_ARITHIGNORE
     case CS_OPT_ARITHIGNORE:
+#endif
+#ifdef CS_OPT_CHAINXACTS
     case CS_OPT_CHAINXACTS:
+#endif
+#ifdef CS_OPT_CURCLOSEONXACT
     case CS_OPT_CURCLOSEONXACT:
+#endif
+#ifdef CS_OPT_FIPSFLAG
     case CS_OPT_FIPSFLAG:
+#endif
+#ifdef CS_OPT_FORCEPLAN
     case CS_OPT_FORCEPLAN:
+#endif
+#ifdef CS_OPT_FORMATONLY
     case CS_OPT_FORMATONLY:
+#endif
+#ifdef CS_OPT_GETDATA
     case CS_OPT_GETDATA:
+#endif
+#ifdef CS_OPT_NOCOUNT
     case CS_OPT_NOCOUNT:
+#endif
+#ifdef CS_OPT_NOEXEC
     case CS_OPT_NOEXEC:
+#endif
+#ifdef CS_OPT_PARSEONLY
     case CS_OPT_PARSEONLY:
+#endif
+#ifdef CS_OPT_QUOTED_IDENT
     case CS_OPT_QUOTED_IDENT:
+#endif
+#ifdef CS_OPT_RESTREES
     case CS_OPT_RESTREES:
+#endif
+#ifdef CS_OPT_SHOWPLAN
     case CS_OPT_SHOWPLAN:
+#endif
+#ifdef CS_OPT_STATS_IO
     case CS_OPT_STATS_IO:
+#endif
+#ifdef CS_OPT_STATS_TIME
     case CS_OPT_STATS_TIME:
+#endif
+#ifdef CS_OPT_STR_RTRUNC
     case CS_OPT_STR_RTRUNC:
+#endif
+#ifdef CS_OPT_TRUNCIGNORE
     case CS_OPT_TRUNCIGNORE:
+#endif
 	return OPTION_BOOL;
+#ifdef CS_OPT_DATEFIRST
     case CS_OPT_DATEFIRST:
+#endif
+#ifdef CS_OPT_DATEFORMAT
     case CS_OPT_DATEFORMAT:
+#endif
+#ifdef CS_OPT_ISOLATION
     case CS_OPT_ISOLATION:
+#endif
+#ifdef CS_OPT_ROWCOUNT
     case CS_OPT_ROWCOUNT:
+#endif
+#ifdef CS_OPT_TEXTSIZE
     case CS_OPT_TEXTSIZE:
+#endif
 	return OPTION_INT;
+#ifdef CS_OPT_AUTHOFF
     case CS_OPT_AUTHOFF:
+#endif
+#ifdef CS_OPT_AUTHON
     case CS_OPT_AUTHON:
+#endif
+#ifdef CS_OPT_CURREAD
     case CS_OPT_CURREAD:
+#endif
+#ifdef CS_OPT_CURWRITE
     case CS_OPT_CURWRITE:
+#endif
+#ifdef CS_OPT_IDENTITYOFF
     case CS_OPT_IDENTITYOFF:
+#endif
+#ifdef CS_OPT_IDENTITYON
     case CS_OPT_IDENTITYON:
+#endif
 	return OPTION_STRING;
     default:
 	return OPTION_UNKNOWN;
