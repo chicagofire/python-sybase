1868f401047ee415309357f534b6f7f06d6e927c
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2002-10-28 05:04:16 +0000

    Adjust setup.py to use sdist target. Copy distribution process from Albatross. Copy LICENSE from Albatross and adjust source code.

--- numeric.c
+++ numeric.c
@@ -1,24 +1,10 @@
-/**********************************************************************
-Copyright 1999 by Object Craft
+/******************************************************************
 
-                        All Rights Reserved
+ Copyright 2001 by Object Craft P/L, Melbourne, Australia.
 
-Permission to use, copy, modify, and distribute this software and its
-documentation for any purpose and without fee is hereby granted,
-provided that the above copyright notice appear in all copies and that
-both that copyright notice and this permission notice appear in
-supporting documentation, and that the name of Object Craft not be
-used in advertising or publicity pertaining to distribution of the
-software without specific, written prior permission.
+ LICENCE - see LICENCE file distributed with this software for details.
 
-OBJECT CRAFT DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
-INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
-EVENT SHALL OBJECT CRAFT BE LIABLE FOR ANY SPECIAL, INDIRECT OR
-CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
-USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-**********************************************************************/
+******************************************************************/
 
 #include "sybasect.h"
 
