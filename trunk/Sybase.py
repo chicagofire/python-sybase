2f1e79392de477c34421a53ed612de4103577a93
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2005-03-19 03:29:35 +0000

    Ready for 0.37pre1 release.

--- trunk/Sybase.py
+++ trunk/Sybase.py
@@ -22,7 +22,7 @@ from sybasect import __have_freetds__
 
 set_debug(sys.stderr)
 
-__version__ = '0.36'
+__version__ = '0.37pre1'
 
 # DB-API values
 apilevel = '2.0'                        # DB API level supported
