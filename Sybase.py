5cf37d7a8863355d6ec0d035217664fc5a0c0dc4
djc <djc@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2005-03-20 22:54:32 +0000

    Bump release to 0.37pre2

--- Sybase.py
+++ Sybase.py
@@ -22,7 +22,7 @@ from sybasect import __have_freetds__
 
 set_debug(sys.stderr)
 
-__version__ = '0.37pre1'
+__version__ = '0.37pre2'
 
 # DB-API values
 apilevel = '2.0'                        # DB API level supported
