826a851cfd66eaca621a1073847af5837a2bf90b
sable <sable@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2006-12-08 18:28:07 +0000

    Changing to release 0.38pre1

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ class my_sdist(sdist):
         sdist.run(self)
 
 setup(name="sybase",
-      version="0.37",
+      version="0.38pre1",
       maintainer="Dave Cole",
       maintainer_email="djc@object-craft.com.au",
       description="Sybase Extension to Python",
