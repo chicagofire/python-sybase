04876210265ee621a15ae4b6a6c6265314730b46
sable <sable@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2007-06-15 12:21:10 +0000

    Added script for bamboo continuous integration

---
+++ dist/bamboo.sh
@@ -0,0 +1,52 @@
+#!/bin/ksh
+
+ARCH=$(./config.guess)
+export ARCH
+SUPPORT_HOME=/livraison/test/${ARCH}/support/support-2.6.5
+export SUPPORT_HOME
+PATH=${SUPPORT_HOME}/bin:${PATH}
+
+case "${ARCH}" in
+    hppa2.0w-hp-hpux11.23)
+        ;;
+    powerpc-ibm-aix5.3.0.0)
+        export LIBPATH=${SUPPORT_HOME}/lib:${LIBPATH}
+        export SYBASE=/sybase/product/15.0
+        if [[ -f ${SYBASE}/SYBASE.sh ]]
+        then
+            . ${SYBASE}/SYBASE.sh
+        fi
+        ;;
+    sparc-sun-solaris2.10)
+        export PATH=/opt/SUNWspro/bin:/usr/local/bin:/usr/sfw/bin:/usr/ccs/bin:/usr/xpg4/bin:$PATH
+        export CC=cc
+        export CFLAGS="-g -O"
+        export MAKE=gmake
+        export ABI=32
+
+        export LD_LIBRARY_PATH=${SUPPORT_HOME}/lib:${LD_LIBRARY_PATH}
+        export SYBASE=/sybase/product/15.0
+        if [[ -f ${SYBASE}/SYBASE.sh ]]
+        then
+            . ${SYBASE}/SYBASE.sh
+        fi
+        ;;
+    *)
+        export LD_LIBRARY_PATH=${SUPPORT_HOME}/lib:${LD_LIBRARY_PATH}
+        export SYBASE=/sybase/product/12.5
+        if [[ -f ${SYBASE}/SYBASE.sh ]]
+        then
+            . ${SYBASE}/SYBASE.sh
+        fi
+        ;;
+esac
+        unset LANG
+
+
+
+cd ..
+python setup.py install --prefix=./target
+PYTHONPATH=./target:${PYTHONPATH}
+export PYTHONPATH
+
+nosetests --source-folder=. --xml-report-folder=./nosexunit-xml --with-nosexunit -v
