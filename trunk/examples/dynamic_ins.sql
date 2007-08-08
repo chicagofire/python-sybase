a49ecda517fa3c122cf158059f092520e8cc23f9
sable <sable@87fcedfa-0024-0410-a646-8dfc3a29dab2>
2007-08-08 11:54:19 +0000

    Moving branche ct_cursor to trunk

--- trunk/examples/dynamic_ins.sql
+++
@@ -1,9 +0,0 @@
-use tempdb
-go
-if exists ( select 1 from sysobjects where name = 'test' and type = 'U' )
-        drop table test
-go
-create table test(col1 int null, 
-		  col2 char(25) null)
-go
-
--- trunk/examples/mult_text.sql
+++
@@ -1,9 +0,0 @@
-
-use tempdb
-go
-if exists ( select 1 from sysobjects where name = 'test' )
-	drop table test
-go
-create table test(col1 int, col2 text, col3 text)
-go
-
